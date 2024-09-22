fee002c597c4d64ead118e8e957d0978b5e526d9
GodOfGenjutsu <160467389+GodOfGenjutsu@users.noreply.github.com>
2024-09-22 21:20:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,5 @@ def predict():
     return jsonify({"prediction": int(predicted_class[0])})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=4000)
+    server_port = int(os.environ.get("PORT", 3050))
+    app.run(debug=True, host="0.0.0.0", port=server_port)
